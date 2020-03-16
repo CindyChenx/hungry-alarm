468742f27dc536fb3ce5fe279d7e7485c9edcd4e
Helentsai <pupu.her@hotmail.com>
2020-03-16 17:55:26 -0400

    react stucture

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+ReactDOM.render(<h1>this is an app for people who are looking to food</h1>, document.getElementById('root'));
+
+
